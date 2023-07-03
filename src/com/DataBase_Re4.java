0eaae4abc30078cd327c2c3ac04ace26cced149d
yixiaochuan <3183023740@qq.com>
2023-07-03 10:51:41 +0800

    hot-fix

--- src/com/DataBase_Re4.java
+++ src/com/DataBase_Re4.java
@@ -8,6 +8,6 @@ public class DataBase_Re4 {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
-        System.out.println(3);
+        System.out.println(3+"hotfix");
     }
 }
